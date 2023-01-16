9822ce706872d36d97cda9f569fdfeced5229361
Akarawat Panwilai <akarawat.bis@gmail.com>
2023-01-16 11:38:42 +0700

    Update find picture multi mixing objects

--- samples/python/find_obj_multi2.3.py
+++ samples/python/find_obj_multi2.3.py
@@ -52,7 +52,7 @@ def main():
         # Converting them to grayscale
         img_gray = cv2.cvtColor(img,
         						cv2.COLOR_BGR2GRAY)
-        temp_gray = cv2.cvtColor(temp,
+        temp_gray = cv2.cvtColor(temp, 
         						cv2.COLOR_BGR2GRAY)                
         
         # Passing the image to matchTemplate method
